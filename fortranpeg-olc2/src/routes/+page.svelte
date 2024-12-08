7758bb0363208e5af90882d5a2c6eef7e324bd4f
elianreyes <elianreyes121@gmail.com>
2024-12-07 20:38:24 -0600

    cambio colores

--- fortranpeg-olc2/src/routes/+page.svelte
+++ fortranpeg-olc2/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import parseInput from "$lib/parser";
     import CodeMirror from "svelte-codemirror-editor";
+    import { oneDark } from "@codemirror/theme-one-dark";
 
     let entry = "";
     let output = "";
@@ -10,9 +11,9 @@
     }
 </script>
 
-<div class="p-5 flex flex-col lg:flex-row gap-4 min-h-screen">
-    <!-- Sección izquierda: CodeMirror -->
-    <div class="flex-1 p-4 bg-gray-100 border rounded-lg shadow min-h-[50vh] overflow-auto">
+<div class="p-5 flex flex-col lg:flex-row gap-4 min-h-screen bg-gray-900 text-gray-200">
+
+    <div class="flex-1 p-4 bg-gray-800 border border-gray-700 rounded-lg shadow min-h-[50vh] overflow-auto">
         <CodeMirror
             bind:value={entry}
             on:change={parse}
@@ -20,8 +21,7 @@
         />
     </div>
 
-    <!-- Sección derecha: Salida -->
-    <div class="flex-1 p-4 bg-gray-200 border rounded-lg shadow min-h-[50vh] overflow-auto">
+    <div class="flex-1 p-4 bg-gray-800 border border-gray-700 rounded-lg shadow min-h-[50vh] overflow-auto">
         <pre class="whitespace-pre-wrap">Salida: {output}</pre>
     </div>
 </div>
